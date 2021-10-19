e76fef446dc9e606f0af7ad3c9566988a5f3c0b6
Gabriel-Valin <gabrielvalincontato@gmail.com>
2021-10-19 20:53:10 -0300

    feat: style for messageList component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,41 @@
+import styles from './styles.module.scss'
+
+import logoImage from '../../assets/logo.svg'
+
 export function MessageList () {
     return (
-        <h1>MessageList</h1>
+        <div className={styles.messageListWrapper}>
+            <img src={logoImage} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Chama neném!!!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/Gabriel-Valin.png" alt="" />
+                        </div>
+                        <span>Gabriel Valin</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Chama neném!!!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/Gabriel-Valin.png" alt="" />
+                        </div>
+                        <span>Gabriel Valin</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Chama neném!!!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/Gabriel-Valin.png" alt="" />
+                        </div>
+                        <span>Gabriel Valin</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
